a7a07cd1d341792fd93360e32aa4a2749e2e0969
Patrick Sutcliffe <patrickdsutcliffe@gmail.com>
2023-11-29 14:18:43 -0500

    removed gunk

--- App.js
+++ App.js
@@ -1,37 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { ActivityIndicator, Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 
-const url = 'https://app.ecwid.com/api/v3/83761399/products';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer public_rk68C9yAbGKaiN7D6TNAB3h4r2DFsmQy',
-  },
-};
 
 const handleButtonClick = () => {
   console.log('you pressed a button');
 };
 
-const ecwidAPITest = async () => {
-  try {
-    const response = await fetch(url, options);
-    const json = await response.json;
-    console.log(json.items);
-    setData(json.items);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    setLoading(false);
-  }
-};
+
 
 
 const App = () => {
+
   return (
-    <View><Text>Hello</Text></View>
+    <SafeAreaView><View><Text>Hello</Text><Button title='test' onPress={handleButtonClick}/></View></SafeAreaView>
+    
   );
 };
 
