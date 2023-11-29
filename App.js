5d29e12ceb2e3677e37f1bc3cc998e87d50a7ca4
Patrick Sutcliffe <patrickdsutcliffe@gmail.com>
2023-11-29 10:03:33 -0500

    initializing expo react native build env

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const [data, setData] = useState([]);
+const [isLoading, setLoading] = useState(true);
+const fetch = require('node-fetch');
+
+const url = 'https://app.ecwid.com/api/v3/83761399/products';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer public_rk68C9yAbGKaiN7D6TNAB3h4r2DFsmQy',
+  },
+};
+
+const handleButtonClick = () => {
+  console.log('you pressed a button');
+};
+
+const ecwidAPITest = async () => {
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json;
+    console.log(json.items);
+    setData(json.items);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setLoading(false);
+  }
+};
+
+useEffect(() => {
+  ecwidAPITest();
+}, []);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, padding: 24}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({id}) => id}
+          renderItem={({item}) => (
+            <Text>
+              {item.name}, {item.price}
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
